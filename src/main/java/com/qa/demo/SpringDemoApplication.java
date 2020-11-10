a53a7f81b886e1f0dfd6ee05014df9ca1982f1ed
jharry444 <jordan.harrison@qa.com>
2020-11-10 10:44:17 +0000

    Added Dog repo

--- src/main/java/com/qa/demo/SpringDemoApplication.java
+++ src/main/java/com/qa/demo/SpringDemoApplication.java
@@ -2,12 +2,20 @@ package com.qa.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.qa.demo.persistence.domain.Dog;
+import com.qa.demo.persistence.repo.DogRepo;
 
 @SpringBootApplication
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoApplication.class, args);
+		// bean bag
+		ApplicationContext ac = SpringApplication.run(SpringDemoApplication.class, args);
+		// DEMONSTRATIVE PURPOSES ONLY
+		DogRepo repo = ac.getBean(DogRepo.class);
+		repo.save(new Dog());
 	}
 
 }
---
+++ src/main/java/com/qa/demo/persistence/repo/DogRepo.java
@@ -0,0 +1,11 @@
+package com.qa.demo.persistence.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.demo.persistence.domain.Dog;
+
+@Repository
+public interface DogRepo extends JpaRepository<Dog, Long> {
+
+}
